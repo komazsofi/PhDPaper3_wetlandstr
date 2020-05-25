fc8e5d5d8f093c8712e354fc1f797eabe3d1b8dd
komazsofi <komazsofi@gmail.com>
2020-05-25 16:25:32 +0200

    small changes

--- src/analysis/pole_lidar_str.R
+++ src/analysis/pole_lidar_str.R
@@ -5,6 +5,8 @@ library(tidyr)
 library(ggplot2)
 library(gridExtra)
 library(GGally)
+library(corrplot)
+library(ggpubr)
 
 workdir="C:/Koma/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/Analysis3/"
 setwd(workdir)
@@ -13,51 +15,51 @@ tisza_2m_r=read.csv("tisza_2m_r_v2.csv")
 ferto_2m_r=read.csv("ferto_2m_r_v2.csv")
 balaton_2m_r=read.csv("balaton_2m_r_v2.csv")
 
-tisza_2m_r$type<-"tisza_2m_r_leafoff"
-ferto_2m_r$type<-"ferto_2m_r_leafoff"
-balaton_2m_r$type<-"balaton_2m_r_leafoff"
+tisza_2m_r$type<-"tisza"
+ferto_2m_r$type<-"ferto"
+balaton_2m_r$type<-"balaton"
 
 tisza_2m_r_sub=subset(tisza_2m_r,select=c(14:29,8,9,10,11))
-names(tisza_2m_r_sub)<-c("biomass","openness","pulsepen","pseudowaveform","h90thperc","sigmaz","type","pole_height",
-                                     "sum_pole_contact","pole_lai","pole_fhd")
+names(tisza_2m_r_sub)<-c("MeanZ","Amp_rms","SigmaZ","Lbiomass","echowidth","MedianZ","P95thZ","NegOpp","NrofEchoMax","pdens",
+                         "PSW","PulsePen","rao_FHD","FHD","StdZ","VarZ","pole_height","Sumpole_cont","LAI","pole_FHD")
 
 ferto_2m_r_sub=subset(ferto_2m_r,select=c(14:29,8,9,10,11))
-names(ferto_2m_r_sub)<-c("biomass","openness","pulsepen","pseudowaveform","h90thperc","sigmaz","type","pole_height",
-                         "sum_pole_contact","pole_lai","pole_fhd")
+names(ferto_2m_r_sub)<-c("MeanZ","Amp_rms","SigmaZ","Lbiomass","echowidth","MedianZ","P95thZ","NegOpp","NrofEchoMax","pdens",
+                         "PSW","PulsePen","rao_FHD","FHD","StdZ","VarZ","pole_height","Sumpole_cont","LAI","pole_FHD")
 
 balaton_2m_r_sub=subset(balaton_2m_r,select=c(14:28,8,9,10,11))
-names(balaton_2m_r_sub)<-c("biomass","openness","pulsepen","pseudowaveform","h90thperc","sigmaz","type","pole_height",
-                         "sum_pole_contact","pole_lai","pole_fhd")
+names(balaton_2m_r_sub)<-c("MeanZ","Amp_rms","SigmaZ","Lbiomass","FHD","MedianZ","P95thZ","NegOpp","NrofEchoMax","pdens",
+                           "PSW","PulsePen","rao_FHD","StdZ","VarZ","pole_height","Sumpole_cont","LAI","pole_FHD")
 
 # per lake
 
-data_tisza=tisza_2m_r_sub %>% gather(-c(pl_hght,sm_pl_c,gct_lai,fhd_pol),key = "var", value = "value")
+data_tisza=tisza_2m_r_sub %>% gather(-c(pole_height,Sumpole_cont,LAI,pole_FHD),key = "var", value = "value")
 
-ggplot(data=data_tisza, aes(x=gct_lai , y=value),show.legend = TRUE) +  
-  geom_point() +
-  geom_smooth(method="lm",colour="black",se=FALSE)+
+ggplot(data=data_tisza, aes(x=LAI , y=value),show.legend = TRUE) +  
+  geom_point(color='darkblue') +
+  geom_smooth(method="lm",colour='darkblue',se=FALSE)+
   stat_cor() +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
   ylab("LiDAR metrics") +
   theme(axis.text.x=element_text(angle=45, hjust=1)) 
 
-data_ferto=ferto_2m_r_sub %>% gather(-c(pl_hght,sm_pl_c,gct_lai,fhd_pol),key = "var", value = "value")
+data_ferto=ferto_2m_r_sub %>% gather(-c(pole_height,Sumpole_cont,LAI,pole_FHD),key = "var", value = "value")
 
-ggplot(data=data_ferto, aes(x=gct_lai , y=value),show.legend = TRUE) +  
-  geom_point() +
-  geom_smooth(method="lm",colour="black",se=FALSE)+
+ggplot(data=data_ferto, aes(x=LAI , y=value),show.legend = TRUE) +  
+  geom_point(color='firebrick') +
+  geom_smooth(method="lm",colour="firebrick",se=FALSE)+
   stat_cor() +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
   ylab("LiDAR metrics") +
   theme(axis.text.x=element_text(angle=45, hjust=1)) 
 
-data_balaton=balaton_2m_r_sub %>% gather(-c(pl_hght,sm_pl_c,gct_lai,fhd_pol),key = "var", value = "value")
+data_balaton=balaton_2m_r_sub %>% gather(-c(pole_height,Sumpole_cont,LAI,pole_FHD),key = "var", value = "value")
 
-ggplot(data=data_balaton, aes(x=gct_lai , y=value),show.legend = TRUE) +  
-  geom_point() +
-  geom_smooth(method="lm",colour="black",se=FALSE)+
+ggplot(data=data_balaton, aes(x=LAI , y=value),show.legend = TRUE) +  
+  geom_point(color='darkorange') +
+  geom_smooth(method="lm",colour="darkorange",se=FALSE)+
   stat_cor() +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
