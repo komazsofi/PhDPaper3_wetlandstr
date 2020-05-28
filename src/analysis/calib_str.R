bd5668002427c172e6c8590546e5ec32b06e94e6
komazsofi <komazsofi@gmail.com>
2020-05-28 15:42:36 +0200

    improve vis

--- src/analysis/calib_str.R
+++ src/analysis/calib_str.R
@@ -19,26 +19,13 @@ tisza_2m_r$type<-"tisza"
 ferto_2m_r$type<-"ferto"
 balaton_2m_r$type<-"balaton"
 
-# For LAI
+# Prep data 
 
 tisza_2m_r_sub_LAI=subset(tisza_2m_r,select=c(7,10,21,25))
 names(tisza_2m_r_sub_LAI) <- c("class","LAI","NegOpeness","Pulsepen")
 
 tisza_2m_r_sub_LAI=tisza_2m_r_sub_LAI[tisza_2m_r_sub_LAI$class!="shrub",]
-
-a=ggplot(data=tisza_2m_r_sub_LAI, aes(x=NegOpeness , y=LAI),show.legend = TRUE) +  
-  geom_point(aes(color=class),size=4) +
-  geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
-  stat_cor(size=10) +
-  theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
-
-b=ggplot(data=tisza_2m_r_sub_LAI, aes(x=Pulsepen , y=LAI),show.legend = TRUE) +  
-  geom_point(aes(color=class),size=4) +
-  geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
-  stat_cor(size=10) +
-  theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+tisza_2m_r_sub_LAI=tisza_2m_r_sub_LAI[tisza_2m_r_sub_LAI$class!="tree",]
 
 ferto_2m_r_sub_LAI=subset(ferto_2m_r,select=c(7,10,21,25))
 names(ferto_2m_r_sub_LAI) <- c("class","LAI","NegOpeness","Pulsepen")
@@ -46,48 +33,44 @@ names(ferto_2m_r_sub_LAI) <- c("class","LAI","NegOpeness","Pulsepen")
 ferto_2m_r_sub_LAI=ferto_2m_r_sub_LAI[ferto_2m_r_sub_LAI$class!="shrub",]
 ferto_2m_r_sub_LAI=ferto_2m_r_sub_LAI[ferto_2m_r_sub_LAI$class!="tree",]
 
-aa=ggplot(data=ferto_2m_r_sub_LAI, aes(x=NegOpeness , y=LAI),show.legend = TRUE) +  
-  geom_point(aes(color=class),size=4) +
-  geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
-  stat_cor(size=10) +
-  theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
-
-bb=ggplot(data=ferto_2m_r_sub_LAI, aes(x=Pulsepen , y=LAI),show.legend = TRUE) +  
-  geom_point(aes(color=class),size=4) +
-  geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
-  stat_cor(size=10) +
-  theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
-
 balaton_2m_r_sub_LAI=subset(balaton_2m_r,select=c(7,10,20,24))
 names(balaton_2m_r_sub_LAI) <- c("class","LAI","NegOpeness","Pulsepen")
 
 balaton_2m_r_sub_LAI=balaton_2m_r_sub_LAI[balaton_2m_r_sub_LAI$class!="shrub",]
 balaton_2m_r_sub_LAI=balaton_2m_r_sub_LAI[balaton_2m_r_sub_LAI$class!="tree",]
 
-aaa=ggplot(data=balaton_2m_r_sub_LAI, aes(x=NegOpeness , y=LAI),show.legend = TRUE) +  
+# visualize pole metrics
+lidarstr="Pulsepen"
+field="LAI"
+
+a=ggplot(data=tisza_2m_r_sub_LAI, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=class),size=4) +
   geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
-  stat_cor(size=10) +
+  stat_cor(size=5) +
   theme_minimal(base_size = 17) +
   theme(axis.text.x=element_text(angle=45, hjust=1)) 
 
-bbb=ggplot(data=balaton_2m_r_sub_LAI, aes(x=Pulsepen , y=LAI),show.legend = TRUE) +  
+aa=ggplot(data=ferto_2m_r_sub_LAI, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=class),size=4) +
   geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
-  stat_cor(size=10) +
+  stat_cor(size=5) +
+  theme_minimal(base_size = 17) +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+
+aaa=ggplot(data=balaton_2m_r_sub_LAI, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
+  geom_point(aes(color=class),size=4) +
+  geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
+  stat_cor(size=5) +
   theme_minimal(base_size = 17) +
   theme(axis.text.x=element_text(angle=45, hjust=1)) 
 
 grid.arrange(
-  b,
-  bb,
-  bbb,
+  aaa+ggtitle("Balaton"),
+  aa+ggtitle("Ferto"),
+  a+ggtitle("Tisza"),
   nrow = 1
 )
 
 # corrplot
 cor(balaton_2m_r_sub_LAI[,c(3,4)], balaton_2m_r_sub_LAI[,2],  method = "pearson", use = "complete.obs")
-
 cor.test(tisza_2m_r_sub_LAI[,3], tisza_2m_r_sub_LAI[,2], method = "pearson")
