2578bf60144aa418466ca0e11a8fd55c9bae303a
komazsofi <komazsofi@gmail.com>
2020-06-08 17:34:52 +0200

    add vis R, R2

--- src/analysis/calib_str_inplot.R
+++ src/analysis/calib_str_inplot.R
@@ -28,25 +28,25 @@ tisza_2m_r_sub=subset(tisza_2m_r,select=c(42,8,38,39,41,
 names(tisza_2m_r_sub) <- c("type","class","Height","FHD_bio","Biomass",
                            "Mean_Z","SigmaZ","Lbiomass","Median_Z","P_95","NegOp","NofEmax","PSW","Pulsepen","FHD","Std_Z","Var_Z","Echowidth")
 
-tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="shrub",]
-tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="tree",]
+#tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="shrub",]
+#tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="tree",]
 
 ferto_2m_r_sub=subset(ferto_2m_r,select=c(41,8,37,38,40,
                                           15,17,18,20,21,22,23,25,26,28,29,30,19))
 names(ferto_2m_r_sub) <- c("type","class","Height","FHD_bio","Biomass",
                            "Mean_Z","SigmaZ","Lbiomass","Median_Z","P_95","NegOp","NofEmax","PSW","Pulsepen","FHD","Std_Z","Var_Z","Echowidth")
 
-ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="shrub",]
-ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="tree",]
+#ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="shrub",]
+#ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="tree",]
 
 balaton_2m_r_sub=subset(balaton_2m_r,select=c(40,8,36,37,39,
-                                                     15,17,18,9,20,21,22,24,25,27,28,29,1))
+                                                     15,17,18,19,20,21,22,24,25,27,28,29,1))
 names(balaton_2m_r_sub) <- c("type","class","Height","FHD_bio","Biomass",
                              "Mean_Z","SigmaZ","Lbiomass","Median_Z","P_95","NegOp","NofEmax","PSW","Pulsepen","FHD","Std_Z","Var_Z","Echowidth")
 balaton_2m_r_sub$Echowidth<-NA
 
-balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="shrub",]
-balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="tree",]
+#balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="shrub",]
+#balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="tree",]
 
 merged=rbind(tisza_2m_r_sub,ferto_2m_r_sub,balaton_2m_r_sub)
 data_merged=merged %>% gather(-c(type,class,Height,FHD_bio,Biomass),key = "var", value = "value")
@@ -62,7 +62,7 @@ ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +
   ylab("LiDAR metrics") +
   theme(axis.text.x=element_text(angle=45, hjust=1)) 
 
-ggplot(data=data_merged, aes(x=value , y=Biomass,colour=type),show.legend = TRUE) +  
+ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
   geom_point() +
   geom_smooth(method="lm",colour='darkblue',se=FALSE)+
   stat_cor() +
@@ -72,6 +72,15 @@ ggplot(data=data_merged, aes(x=value , y=Biomass,colour=type),show.legend = TRUE
   theme(axis.text.x=element_text(angle=45, hjust=1)) 
 
 ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +  
+  geom_point() +
+  geom_smooth(method="lm",colour='darkblue',se=FALSE)+
+  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~"))) +
+  facet_wrap(~var,scales = "free") +
+  theme_minimal() +
+  ylab("LiDAR metrics") +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+
+ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
   geom_point() +
   geom_smooth(method="lm",colour='darkblue',se=FALSE)+
   stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~"))) +
