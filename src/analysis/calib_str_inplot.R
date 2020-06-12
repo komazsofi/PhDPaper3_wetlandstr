50c70bae44155988a78690512677d0e7403346c3
komazsofi <komazsofi@gmail.com>
2020-06-12 12:42:50 +0200

    cont. analysis quadrat

--- src/analysis/calib_str_inplot.R
+++ src/analysis/calib_str_inplot.R
@@ -26,64 +26,61 @@ balaton_2m_r$type<-"balaton"
 tisza_2m_r_sub=subset(tisza_2m_r,select=c(42,8,38,39,41,
                                           15,17,18,20,21,22,23,25,26,28,29,30,19))
 names(tisza_2m_r_sub) <- c("type","class","Height","FHD_bio","Biomass",
-                           "Mean_Z","SigmaZ","Lbiomass","Median_Z","H_95p","NegOp","NofEmax","PSW","Pulsepen","VV_fhd","Std_Z","Var_Z","Echowidth")
+                           "H_m","VV_sigmaz","VV_lbio","H_50p","H_95p","C_negop","VV_nofech","VV_psw","C_ppr","VV_fhd","VV_std","VV_var","VV_echw")
 
-#tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="shrub",]
-#tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="tree",]
+tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="shrub",]
+tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="tree",]
+tisza_2m_r_sub=tisza_2m_r_sub[tisza_2m_r_sub$class!="grassland",]
+tisza_2m_r_sub=tisza_2m_r_sub[is.na(tisza_2m_r_sub$class)==FALSE,]
 
 ferto_2m_r_sub=subset(ferto_2m_r,select=c(41,8,37,38,40,
                                           15,17,18,20,21,22,23,25,26,28,29,30,19))
 names(ferto_2m_r_sub) <- c("type","class","Height","FHD_bio","Biomass",
-                           "Mean_Z","SigmaZ","Lbiomass","Median_Z","H_95p","NegOp","NofEmax","PSW","Pulsepen","VV_fhd","Std_Z","Var_Z","Echowidth")
+                           "H_m","VV_sigmaz","VV_lbio","H_50p","H_95p","C_negop","VV_nofech","VV_psw","C_ppr","VV_fhd","VV_std","VV_var","VV_echw")
 
-#ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="shrub",]
-#ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="tree",]
+ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="shrub",]
+ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="tree",]
+ferto_2m_r_sub=ferto_2m_r_sub[ferto_2m_r_sub$class!="grassland",]
 
 balaton_2m_r_sub=subset(balaton_2m_r,select=c(40,8,36,37,39,
                                                      15,17,18,19,20,21,22,24,25,27,28,29,1))
 names(balaton_2m_r_sub) <- c("type","class","Height","FHD_bio","Biomass",
-                             "Mean_Z","SigmaZ","Lbiomass","Median_Z","H_95p","NegOp","NofEmax","PSW","Pulsepen","VV_fhd","Std_Z","Var_Z","Echowidth")
-balaton_2m_r_sub$Echowidth<-NA
+                             "H_m","VV_sigmaz","VV_lbio","H_50p","H_95p","C_negop","VV_nofech","VV_psw","C_ppr","VV_fhd","VV_std","VV_var","VV_echw")
+balaton_2m_r_sub$VV_echw<-NA
 
-#balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="shrub",]
-#balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="tree",]
+balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="shrub",]
+balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="tree",]
+balaton_2m_r_sub=balaton_2m_r_sub[balaton_2m_r_sub$class!="grassland",]
 
 merged=rbind(tisza_2m_r_sub,ferto_2m_r_sub,balaton_2m_r_sub)
 data_merged=merged %>% gather(-c(type,class,Height,FHD_bio,Biomass),key = "var", value = "value")
 
 # which one?
 
-ggplot(data=data_merged, aes(x=Height , y=value),show.legend = TRUE) +  
+ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +  
   geom_point(aes(colour=type)) +
   geom_smooth(method = "gam")+
   stat_cor(method = "spearman") +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
-  ylab("LiDAR metrics") +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
-
-ggplot(data=data_merged, aes(x=Height , y=value),show.legend = TRUE) +  
-  geom_point(aes(colour=class)) +
-  geom_smooth(method = "gam")+
-  stat_cor(method = "spearman") +
-  facet_wrap(~var,scales = "free") +
-  theme_minimal() +
-  ylab("LiDAR metrics") +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
-
-ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
-  geom_point() +
-  geom_smooth(method = "gam")+
-  stat_cor(method = "spearman") +
-  facet_wrap(~var,scales = "free") +
-  theme_minimal() +
-  ylab("LiDAR metrics") +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+  ylab("Height") +
+  xlab("LiDAR metrics") +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+ggtitle("Spearman correlation coefficient")
 
 ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +  
+  geom_point(aes(colour=type)) +
+  geom_smooth(method = "lm")+
+  stat_cor(method = "pearson") +
+  facet_wrap(~var,scales = "free") +
+  theme_minimal() +
+  ylab("Height") +
+  xlab("LiDAR metrics") +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+ggtitle("Pearson correlation coefficient")
+
+ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
   geom_point() +
-  geom_smooth(method="lm",colour='darkblue',se=FALSE)+
-  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~"))) +
+  geom_smooth(method = "gam")+
+  stat_cor(method = "spearman") +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
   ylab("LiDAR metrics") +
@@ -91,8 +88,8 @@ ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +
 
 ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
   geom_point() +
-  geom_smooth(method="lm",colour='darkblue',se=FALSE)+
-  stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~"))) +
+  geom_smooth(method = "lm")+
+  stat_cor(method = "pearson") +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
   ylab("LiDAR metrics") +
@@ -107,21 +104,21 @@ a=ggplot(data=tisza_2m_r_sub, aes_string(x=lidarstr , y=field),show.legend = TRU
   geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
   stat_cor(size=5) +
   theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+  theme(axis.text.x=element_text(angle=45, hjust=1))+xlim(0,2)+ylim(0,5) 
 
 aa=ggplot(data=ferto_2m_r_sub, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=class),size=4) +
   geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
   stat_cor(size=5) +
   theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+  theme(axis.text.x=element_text(angle=45, hjust=1))+xlim(0,2)+ylim(0,5)
 
 aaa=ggplot(data=balaton_2m_r_sub, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=class),size=4) +
   geom_smooth(method="lm",colour='darkblue',se=FALSE,size=4)+
   stat_cor(size=5) +
   theme_minimal(base_size = 17) +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+  theme(axis.text.x=element_text(angle=45, hjust=1))+xlim(0,2)+ylim(0,5)
 
 grid.arrange(
   aaa+ggtitle("Balaton"),
