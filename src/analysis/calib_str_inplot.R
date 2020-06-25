9581e1dd71d7e89d6c15233775ca7bcd43e3a365
komazsofi <komazsofi@gmail.com>
2020-06-25 15:21:59 +0200

    add analysis plot

--- src/analysis/calib_str_inplot.R
+++ src/analysis/calib_str_inplot.R
@@ -17,27 +17,31 @@ tisza_2m_r=read.csv("tisza_plot_edited.csv")
 ferto_2m_r=read.csv("ferto_plot_edited.csv")
 balaton_2m_r=read.csv("balaton_plot_edited.csv")
 
-tisza_2m_r$type<-"tisza"
-ferto_2m_r$type<-"ferto"
-balaton_2m_r$type<-"balaton"
+tisza_2m_r$type<-"FWF"
+ferto_2m_r$type<-"FWF"
+balaton_2m_r$type<-"non-FWF"
+
+tisza_2m_r$lake<-"Lake Tisza"
+ferto_2m_r$lake<-"Lake Ferto"
+balaton_2m_r$lake<-"Lake Balaton"
 
 # Prep data 
 
-tisza_2m_r_sub=subset(tisza_2m_r,select=c(42,8,38,39,41,7,13,14,
+tisza_2m_r_sub=subset(tisza_2m_r,select=c(43,8,38,39,41,7,13,14,42,
                                           15,17,18,20,21,22,23,25,26,28,29,30,19))
-names(tisza_2m_r_sub) <- c("lake","class","Height","FHD_bio","Biomass","type","x","y",
+names(tisza_2m_r_sub) <- c("lake","class","Height","FHD_bio","Biomass","veg_type","x","y","type",
                            "H_m","VV_sigmaz","VV_lbio","H_50p","H_95p","C_negop","VV_nofech","VV_psw","C_ppr","VV_fhd","VV_std","VV_var","VV_echw")
 
 tisza_2m_r_sub=tisza_2m_r_sub[is.na(tisza_2m_r_sub$class)==FALSE,]
 
-ferto_2m_r_sub=subset(ferto_2m_r,select=c(41,8,37,38,40,7,13,14,
+ferto_2m_r_sub=subset(ferto_2m_r,select=c(42,8,37,38,40,7,13,14,41,
                                           15,17,18,20,21,22,23,25,26,28,29,30,19))
-names(ferto_2m_r_sub) <- c("lake","class","Height","FHD_bio","Biomass","type","x","y",
+names(ferto_2m_r_sub) <- c("lake","class","Height","FHD_bio","Biomass","veg_type","x","y","type",
                            "H_m","VV_sigmaz","VV_lbio","H_50p","H_95p","C_negop","VV_nofech","VV_psw","C_ppr","VV_fhd","VV_std","VV_var","VV_echw")
 
-balaton_2m_r_sub=subset(balaton_2m_r,select=c(40,8,36,37,39,7,13,14,
+balaton_2m_r_sub=subset(balaton_2m_r,select=c(41,8,36,37,39,7,13,14,40,
                                                      15,17,18,19,20,21,22,24,25,27,28,29,1))
-names(balaton_2m_r_sub) <- c("lake","class","Height","FHD_bio","Biomass","type","x","y",
+names(balaton_2m_r_sub) <- c("lake","class","Height","FHD_bio","Biomass","veg_type","x","y","type",
                              "H_m","VV_sigmaz","VV_lbio","H_50p","H_95p","C_negop","VV_nofech","VV_psw","C_ppr","VV_fhd","VV_std","VV_var","VV_echw")
 balaton_2m_r_sub$VV_echw<-NA
 
@@ -64,11 +68,13 @@ balaton_2m_r_sub$class=str_replace(balaton_2m_r_sub$class,"reed","phragmites")
 #merging
 
 merged=rbind(tisza_2m_r_sub,ferto_2m_r_sub,balaton_2m_r_sub)
-data_merged=merged %>% gather(-c(type,class,Height,FHD_bio,Biomass,lake),key = "var", value = "value")
+data_merged=merged %>% gather(-c(veg_type,class,Height,FHD_bio,Biomass,lake,type),key = "var", value = "value")
 
 # which one?
 
-ggplot(data=data_merged, aes(x=value , y=Biomass),show.legend = TRUE) +  
+# all together
+
+ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +  
   geom_point(aes(colour=class)) +
   geom_smooth(method = "gam")+
   stat_cor(method = "spearman") +
@@ -79,7 +85,7 @@ ggplot(data=data_merged, aes(x=value , y=Biomass),show.legend = TRUE) +
   theme(axis.text.x=element_text(angle=45, hjust=1))+ggtitle("Spearman correlation coefficient")
 
 ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +  
-  geom_point(aes(colour=type)) +
+  geom_point(aes(colour=class)) +
   geom_smooth(method = "lm")+
   stat_cor(method = "pearson") +
   facet_wrap(~var,scales = "free") +
@@ -88,14 +94,17 @@ ggplot(data=data_merged, aes(x=value , y=Height),show.legend = TRUE) +
   xlab("LiDAR metrics") +
   theme(axis.text.x=element_text(angle=45, hjust=1))+ggtitle("Pearson correlation coefficient")
 
+# FWF, non-FWF
+
 ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
   geom_point() +
   geom_smooth(method = "gam")+
   stat_cor(method = "spearman") +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
-  ylab("LiDAR metrics") +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+  ylab("Height") +
+  xlab("LiDAR metrics") +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+ggtitle("Spearman correlation coefficient")
 
 ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE) +  
   geom_point() +
@@ -103,12 +112,13 @@ ggplot(data=data_merged, aes(x=value , y=Height,colour=type),show.legend = TRUE)
   stat_cor(method = "pearson") +
   facet_wrap(~var,scales = "free") +
   theme_minimal() +
-  ylab("LiDAR metrics") +
-  theme(axis.text.x=element_text(angle=45, hjust=1)) 
+  ylab("Height") +
+  xlab("LiDAR metrics") +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+ggtitle("Pearson correlation coefficient")
 
-# visualize pole metrics
-lidarstr="VV_sigmaz"
-field="Biomass"
+# per lakes
+lidarstr="H_95p"
+field="Height"
 
 a=ggplot(data=tisza_2m_r_sub, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=class),size=4) +
@@ -187,29 +197,81 @@ grid.arrange(
   a+ggtitle("Tisza"),
   nrow = 1
 )
+
+# FWF, non-FWF
+lidarstr="H_95p"
+field="Height"
 
 merged$id=seq(1,34,1)
 
 ggplot(data=merged, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=lake,shape=class),size=4) +
-  geom_smooth(method="lm",formula=y~poly(x,1),colour='darkblue',size=2,se=FALSE)+
-  geom_smooth(se=FALSE)+
+  geom_smooth(se=TRUE)+
   stat_cor(method = "spearman",size=5) +
   theme_minimal(base_size = 17) +
   theme(axis.text.x=element_text(angle=45, hjust=1))+
-  geom_text(aes(label=id),hjust=0, vjust=0)
+  geom_text(aes(label=id),hjust=0, vjust=0) +
+  ggtitle("Spearman correlation coefficient")
 
-write.csv(merged,"Plot_merged_cleaned.csv")
-
-merged_sub=merged[merged$lake!="balaton",]
+merged_sub=merged[merged$lake!="Lake Balaton",]
 
 ggplot(data=merged_sub, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
   geom_point(aes(color=lake,shape=class),size=4) +
-  geom_smooth(method="lm",formula=y~poly(x,1),colour='darkblue',size=2,se=FALSE)+
-  geom_smooth(se=FALSE)+
+  geom_smooth(se=TRUE)+
   stat_cor(method = "spearman",size=5) +
   theme_minimal(base_size = 17) +
   theme(axis.text.x=element_text(angle=45, hjust=1))+
-  geom_text(aes(label=id),hjust=0, vjust=0)
+  geom_text(aes(label=id),hjust=0, vjust=0)+
+  ggtitle("Spearman correlation coefficient")
 
+# export
+write.csv(merged,"Plot_merged_cleaned.csv")
 
+# Extra attributes
+
+merged_withextra=read.csv("Plot_merged_cleaned2.csv")
+
+ggplot(data=merged_withextra, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
+  geom_point(aes(color=density,shape=composition),size=4) +
+  geom_smooth(se=TRUE)+
+  stat_cor(method = "spearman",size=5) +
+  theme_minimal(base_size = 17) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  geom_text(aes(label=id),hjust=0, vjust=0) +
+  ggtitle("Spearman correlation coefficient")
+
+ggplot(data=merged_withextra, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
+  geom_point(aes(color=age,shape=composition),size=4) +
+  geom_smooth(se=TRUE)+
+  stat_cor(method = "spearman",size=5) +
+  theme_minimal(base_size = 17) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  geom_text(aes(label=id),hjust=0, vjust=0) +
+  ggtitle("Spearman correlation coefficient")
+
+ggplot(data=merged_withextra, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
+  geom_point(aes(color=knick,shape=composition),size=4) +
+  geom_smooth(se=TRUE)+
+  stat_cor(method = "spearman",size=5) +
+  theme_minimal(base_size = 17) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  geom_text(aes(label=id),hjust=0, vjust=0) +
+  ggtitle("Spearman correlation coefficient")
+
+ggplot(data=merged_withextra, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
+  geom_point(aes(color=ruderal,shape=composition),size=4) +
+  geom_smooth(se=TRUE)+
+  stat_cor(method = "spearman",size=5) +
+  theme_minimal(base_size = 17) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  geom_text(aes(label=id),hjust=0, vjust=0) +
+  ggtitle("Spearman correlation coefficient")
+
+ggplot(data=merged_withextra, aes_string(x=lidarstr , y=field),show.legend = TRUE) +  
+  geom_point(aes(color=dieback,shape=composition),size=4) +
+  geom_smooth(se=TRUE)+
+  stat_cor(method = "spearman",size=5) +
+  theme_minimal(base_size = 17) +
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  geom_text(aes(label=id),hjust=0, vjust=0) +
+  ggtitle("Spearman correlation coefficient")
