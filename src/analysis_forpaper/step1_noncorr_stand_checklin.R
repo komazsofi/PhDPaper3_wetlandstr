49d1ceb81b9d468792d11a3d3c7d055a1caf3135
komazsofi <komazsofi@gmail.com>
2020-09-17 10:15:28 +0200

    add standardization

--- src/analysis_forpaper/step1_noncorr_stand_checklin.R
+++ src/analysis_forpaper/step1_noncorr_stand_checklin.R
@@ -20,12 +20,28 @@ for(i in rad) {
   vifcorr_pole=vifcor(pole_data[,c(4:17,24)], th=0.6, method='spearman')
   print(vifcorr_pole)
   
+  pole_noncorr=exclude(pole_data,vifcorr_pole)
+  scale_pole_noncorr=scale(pole_noncorr)
+  colnames(scale_pole_noncorr)=paste("Scaled_",colnames(scale_pole_noncorr),sep="")
+  
+  pole_noncor_full=cbind(pole_noncorr,pole_data[,c(2,18,19,20,21,22,23,25)],scale_pole_noncorr)
+  write.csv(pole_noncor_full,paste("Pole_noncorr",i,".csv",sep=""))
+  
+  ###################
+  
   print(paste("Plot: selected radius is:",i,"m",sep=""))
   
   plot_data=read.csv(paste("Plot_db_",i,"_filt.csv",sep=""))
   
-  vifcorr_plot=vifcor(pole_data[,c(3:16,24)], th=0.6, method='spearman')
+  vifcorr_plot=vifcor(plot_data[,c(3:16,24)], th=0.6, method='spearman')
   print(vifcorr_plot)
+  
+  plot_noncorr=exclude(plot_data,vifcorr_plot)
+  scale_plot_noncorr=scale(plot_noncorr)
+  colnames(scale_plot_noncorr)=paste("Scaled_",colnames(scale_plot_noncorr),sep="")
+  
+  plot_noncor_full=cbind(plot_noncorr,plot_data[,c(2,17,18,19,20,21,22,23,25,26,27)],scale_plot_noncorr)
+  write.csv(plot_noncor_full,paste("Plot_noncorr",i,".csv",sep=""))
   
 }
 
