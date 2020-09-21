74ca0f6b35d2307fe4ca36c3b3311b37dc8d3581
komazsofi <komazsofi@gmail.com>
2020-09-21 15:15:07 +0200

    add lai

--- src/analysis_forpaper/step2_lincheck_regression.R
+++ src/analysis_forpaper/step2_lincheck_regression.R
@@ -271,3 +271,188 @@ model_nfwf_step_b5<-step(model_nfwf_b5,direction = "backward")
 summary(model_nfwf_step_b5)
 
 stargazer(model_nfwf_step_h5, model_fwf_step_h5, model_all_h5, model_nfwf_step_b5, model_fwf_step_b5, model_all_step_b5,align=TRUE,type="html",column.labels=c("discrete","fwf","all","discrete","fwf","all"),title="5 m",out="height_biomass_5.doc")
+
+####################################### Pole
+
+pole_data05=read.csv(paste("Pole_noncorr",0.5,".csv",sep=""))
+pole_data2=read.csv(paste("Pole_noncorr",2,".csv",sep=""))
+pole_data5=read.csv(paste("Pole_noncorr",5,".csv",sep=""))
+
+##### Visualization
+
+# 0.5 m
+
+pole_data05_c=pole_data05[c(2:6,7,9,10,13,14)]
+pole_data05_c[pole_data05_c$W_echw==0,5]<- NA
+pole_data05_c_vis=pole_data05_c %>% gather(-c(gct_lai,lake,class,nofallp,OBJNAME),key = "var", value = "value")
+
+ggplot(data=pole_data05_c_vis, aes(x=value , y=gct_lai),show.legend = TRUE) +  
+  geom_point(aes(colour=lake,shape=class),size=2.5) +
+  facet_wrap(~var,scales = "free") +
+  theme_bw() +
+  ylab("Leaf area (field)") +
+  xlab("LiDAR metrics")+
+  geom_text(aes(label=OBJNAME),hjust=0, vjust=0,size=2)+
+  ggtitle("Non-correlated metrics for estimating leaf area (0.5 m)")+
+  scale_colour_manual(values=c("Lake Balaton"="red", "Lake Ferto"="darkgreen","Lake Tisza"="blue"),name="Lakes")+
+  scale_shape_manual(values=c("carex"=16,"phragmites"=17,"typha"=15),name="Species",labels=c("Carex spec.","Phragmites australis","Typha spec."))
+
+# all
+model_all_lai05=lm(gct_lai ~ A_med+C_ppr+V_ku+V_var, data = pole_data05)
+summary(model_all_lai05) 
+
+#AIC model selection (step)
+model_all_lai05_step<-step(model_all_lai05,direction = "backward")
+summary(model_all_lai05_step)
+
+# fwf high
+model_fwfh_lai05=lm(gct_lai ~ A_med+C_ppr+V_ku+V_var+W_echw, data = pole_data05[pole_data05$lake=="Lake Tisza",])
+summary(model_fwfh_lai05) 
+
+#AIC model selection (step)
+model_fwfh_lai05_step<-step(model_fwfh_lai05,direction = "backward")
+summary(model_fwfh_lai05_step)
+
+# fwf low
+model_fwfl_lai05=lm(gct_lai ~ A_med+C_ppr+V_ku+V_var+W_echw, data = pole_data05[pole_data05$lake=="Lake Ferto",])
+summary(model_fwfl_lai05) 
+
+#AIC model selection (step)
+model_fwfl_lai05_step<-step(model_fwfl_lai05,direction = "backward")
+summary(model_fwfl_lai05_step)
+
+# fwf all
+model_fwf_lai05=lm(gct_lai ~ A_med+C_ppr+V_ku+V_var+W_echw, data = pole_data05[pole_data05$lake!= "Lake Balaton",])
+summary(model_fwf_lai05) 
+
+#AIC model selection (step)
+model_fwf_lai05_step<-step(model_fwf_lai05,direction = "backward")
+summary(model_fwf_lai05_step)
+
+# non-fwf
+model_fwfn_lai05=lm(gct_lai ~ A_med+C_ppr+V_ku+V_var, data = pole_data05[pole_data05$lake=="Lake Balaton",])
+summary(model_fwfn_lai05) 
+
+#AIC model selection (step)
+model_fwfn_lai05_step<-step(model_fwfn_lai05,direction = "backward")
+summary(model_fwfn_lai05_step)
+
+stargazer(model_fwfn_lai05,model_fwfh_lai05_step,model_fwfl_lai05_step,model_fwf_lai05_step,model_all_lai05_step,align=TRUE,type="html",column.labels=c("discrete","fwf (20 pt/m2)","fwf (4 pt/m2)", "fwf","all"),title="0.5 m",out="lai_05.doc")
+
+# 2 m
+
+pole_data2_c=pole_data2[c(2:6,7,9,10,13,14)]
+pole_data2_c[pole_data2_c$W_echw==0,5]<- NA
+pole_data2_c_vis=pole_data2_c %>% gather(-c(gct_lai,lake,class,nofallp,OBJNAME),key = "var", value = "value")
+
+ggplot(data=pole_data2_c_vis, aes(x=value , y=gct_lai),show.legend = TRUE) +  
+  geom_point(aes(colour=lake,shape=class),size=2.5) +
+  facet_wrap(~var,scales = "free") +
+  theme_bw() +
+  ylab("Leaf area (field)") +
+  xlab("LiDAR metrics")+
+  geom_text(aes(label=OBJNAME),hjust=0, vjust=0,size=2)+
+  ggtitle("Non-correlated metrics for estimating leaf area (2 m)")+
+  scale_colour_manual(values=c("Lake Balaton"="red", "Lake Ferto"="darkgreen","Lake Tisza"="blue"),name="Lakes")+
+  scale_shape_manual(values=c("carex"=16,"phragmites"=17,"typha"=15),name="Species",labels=c("Carex spec.","Phragmites australis","Typha spec."))
+
+# all
+model_all_lai2=lm(gct_lai ~ A_med+A_cover+V_ku+V_var, data = pole_data2)
+summary(model_all_lai2) 
+
+#AIC model selection (step)
+model_all_lai2_step<-step(model_all_lai2,direction = "backward")
+summary(model_all_lai2_step)
+
+# fwf high
+model_fwfh_lai2=lm(gct_lai ~ A_med+A_cover+V_ku+V_var+W_echw, data = pole_data2[pole_data2$lake=="Lake Tisza",])
+summary(model_fwfh_lai2) 
+
+#AIC model selection (step)
+model_fwfh_lai2_step<-step(model_fwfh_lai2,direction = "backward")
+summary(model_fwfh_lai2_step)
+
+# fwf low
+model_fwfl_lai2=lm(gct_lai ~ A_med+A_cover+V_ku+V_var+W_echw, data = pole_data2[pole_data2$lake=="Lake Ferto",])
+summary(model_fwfl_lai2) 
+
+#AIC model selection (step)
+model_fwfl_lai2_step<-step(model_fwfl_lai2,direction = "backward")
+summary(model_fwfl_lai2_step)
+
+# fwf all
+model_fwf_lai2=lm(gct_lai ~ A_med+A_cover+V_ku+V_var+W_echw, data = pole_data2[pole_data2$lake!= "Lake Balaton",])
+summary(model_fwf_lai2) 
+
+#AIC model selection (step)
+model_fwf_lai2_step<-step(model_fwf_lai2,direction = "backward")
+summary(model_fwf_lai2_step)
+
+# non-fwf
+model_fwfn_lai2=lm(gct_lai ~ A_med+A_cover+V_ku+V_var, data = pole_data2[pole_data2$lake=="Lake Balaton",])
+summary(model_fwfn_lai2) 
+
+#AIC model selection (step)
+model_fwfn_lai2_step<-step(model_fwfn_lai2,direction = "backward")
+summary(model_fwfn_lai2_step)
+
+stargazer(model_fwfn_lai2,model_fwfh_lai2_step,model_fwfl_lai2_step,model_fwf_lai2_step,model_all_lai2_step,align=TRUE,type="html",column.labels=c("discrete","fwf (20 pt/m2)","fwf (4 pt/m2)", "fwf","all"),title="2 m",out="lai_2.doc")
+
+# 5 m
+
+pole_data5_c=pole_data5[c(2:6,7,9,10,13,14)]
+pole_data5_c[pole_data5_c$W_echw==0,5]<- NA
+pole_data5_c_vis=pole_data5_c %>% gather(-c(gct_lai,lake,class,nofallp,OBJNAME),key = "var", value = "value")
+
+ggplot(data=pole_data5_c_vis, aes(x=value , y=gct_lai),show.legend = TRUE) +  
+  geom_point(aes(colour=lake,shape=class),size=2.5) +
+  facet_wrap(~var,scales = "free") +
+  theme_bw() +
+  ylab("Leaf area (field)") +
+  xlab("LiDAR metrics")+
+  geom_text(aes(label=OBJNAME),hjust=0, vjust=0,size=2)+
+  ggtitle("Non-correlated metrics for estimating leaf area (5 m)")+
+  scale_colour_manual(values=c("Lake Balaton"="red", "Lake Ferto"="darkgreen","Lake Tisza"="blue"),name="Lakes")+
+  scale_shape_manual(values=c("carex"=16,"phragmites"=17,"typha"=15),name="Species",labels=c("Carex spec.","Phragmites australis","Typha spec."))
+
+# all
+model_all_lai5=lm(gct_lai ~ A_med+A_cover+H_max+V_ku, data = pole_data5)
+summary(model_all_lai5) 
+
+#AIC model selection (step)
+model_all_lai5_step<-step(model_all_lai5,direction = "backward")
+summary(model_all_lai5_step)
+
+# fwf high
+model_fwfh_lai5=lm(gct_lai ~ A_med+A_cover+H_max+V_ku+W_echw, data = pole_data5[pole_data5$lake=="Lake Tisza",])
+summary(model_fwfh_lai5) 
+
+#AIC model selection (step)
+model_fwfh_lai5_step<-step(model_fwfh_lai5,direction = "backward")
+summary(model_fwfh_lai5_step)
+
+# fwf low
+model_fwfl_lai5=lm(gct_lai ~ A_med+A_cover+H_max+V_ku+W_echw, data = pole_data5[pole_data5$lake=="Lake Ferto",])
+summary(model_fwfl_lai5) 
+
+#AIC model selection (step)
+model_fwfl_lai5_step<-step(model_fwfl_lai5,direction = "backward")
+summary(model_fwfl_lai5_step)
+
+# fwf all
+model_fwf_lai5=lm(gct_lai ~ A_med+A_cover+H_max+V_ku+W_echw, data = pole_data5[pole_data5$lake!= "Lake Balaton",])
+summary(model_fwf_lai5) 
+
+#AIC model selection (step)
+model_fwf_lai5_step<-step(model_fwf_lai5,direction = "backward")
+summary(model_fwf_lai5_step)
+
+# non-fwf
+model_fwfn_lai5=lm(gct_lai ~ A_med+A_cover+H_max+V_ku, data = pole_data5[pole_data5$lake=="Lake Balaton",])
+summary(model_fwfn_lai5) 
+
+#AIC model selection (step)
+model_fwfn_lai5_step<-step(model_fwfn_lai5,direction = "backward")
+summary(model_fwfn_lai5_step)
+
+stargazer(model_fwfn_lai5,model_fwfh_lai5_step,model_fwfl_lai5_step,model_fwf_lai5_step,model_all_lai5_step,align=TRUE,type="html",column.labels=c("discrete","fwf (20 pt/m2)","fwf (4 pt/m2)", "fwf","all"),title="5 m",out="lai_5.doc")
