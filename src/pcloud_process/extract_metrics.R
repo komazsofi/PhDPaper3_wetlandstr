fd85bdfed3de9962a936916dfa032987a83bc919
komazsofi <komazsofi@gmail.com>
2020-09-24 15:30:34 +0200

    clean up features and scale amplitude

--- src/pcloud_process/extract_metrics.R
+++ src/pcloud_process/extract_metrics.R
@@ -7,15 +7,15 @@ library(sp)
 library(e1071)
 
 # Set working dirctory
-#workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/pcloud/balaton_25mrad_reclass/"
+workingdirectory="C:/Koma/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/pcloud/balaton_25mrad_reclass/"
 #workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/pcloud/tisza_25mrad_reclass/"
 #workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/pcloud/tisza_25mrad_leafon_reclass/"
-workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/pcloud/ferto_25mrad_reclass/"
+#workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter2_habitat_str_lidar/3_Dataprocessing/pcloud/ferto_25mrad_reclass/"
 setwd(workingdirectory)
 
-#fieldfile="w_point_balaton.shp"
+fieldfile="w_point_balaton.shp"
 #fieldfile="tisza_full.shp"
-fieldfile="w_point.shp"
+#fieldfile="w_point.shp"
 
 #Import
 fieldsp = readOGR(dsn=fieldfile)
@@ -23,15 +23,15 @@ fieldsp_df=fieldsp@data
 
 objname=fieldsp_df$OBJNAME
 
-dpcloudfea_exp_df <- data.frame(matrix(ncol = 19, nrow = 0))
-x <- c("OBJNAME", "point_ID","point_name", "H_max","H_mean","H_median","H_q25","H_q75","H_q95",
-       "V_std","V_var","V_sk","V_ku","C_ppr","A_std","A_med","A_cover","nofveg","nofallp")
+dpcloudfea_exp_df <- data.frame(matrix(ncol = 13, nrow = 0))
+x <- c("OBJNAME", "point_ID","point_name", "H_p99","H_mean","H_median",
+       "V_std","V_var","C_ppr","A_std","A_cover","nofveg","nofallp")
 colnames(dpcloudfea_exp_df) <- x
 
-rad=5
-#name="Balaton_OBJNAME"
+rad=2.5
+name="Balaton_OBJNAME"
 #name="Tisza_OBJNAME"
-name="Ferto_OBJNAME"
+#name="Ferto_OBJNAME"
 
 for (i in objname) {
   
@@ -45,47 +45,35 @@ for (i in objname) {
     areaofint_sel=fieldsp[fieldsp$OBJNAME==i,]
     
     if (nrow(las@data[las@data$Classification==2,])>10) {
-      las_norm=lasnormalize(las, knnidw(k=10,p=2))
+      las_norm=lasnormalize(las, knnidw(k=35,p=1))
       
       las_clip=lasclipCircle(las_norm,areaofint_sel@coords[1],areaofint_sel@coords[2],rad)
       
       las_norm_veg=lasfilter(las_clip,(Classification!=2L))
       
-      H_max= max(las_norm_veg@data$Z)
+      H_p99= quantile(las_norm_veg@data$Z, 0.99)
       H_mean= mean(las_norm_veg@data$Z)
       H_median=median(las_norm_veg@data$Z)
-      H_q25 = quantile(las_norm_veg@data$Z, 0.25)
-      H_q75 = quantile(las_norm_veg@data$Z, 0.75)
-      H_q95 = quantile(las_norm_veg@data$Z, 0.95)
       
       V_std=sd(las_clip@data$Z)
       V_var=var(las_clip@data$Z)
-      V_sk=skewness(las_clip@data$Z)
-      V_ku=kurtosis(las_clip@data$Z)
       
       C_ppr=(nrow(las_clip@data[las_clip@data$Classification==2L])/length(las_clip@data$Z))*100
       
-      A_std=sd(las_clip@data$Intensity)
-      A_med=median(las_clip@data$Intensity)
+      A_std=sd(scale(las_clip@data$Intensity,center = FALSE))
       A_cover=sum(las_norm_veg@data$Intensity)/sum(las_clip@data$Intensity)
       
       nofallp=length(las_clip@data$Z)
       nofveg=length(las_norm_veg@data$Z)
       
       newline <- data.frame(t(c(OBJNAME=i,point_ID=areaofint_sel$point_ID,point_name=as.character(areaofint_sel$point_name),
-                                H_max=H_max,
+                                H_p99=H_p99,
                                 H_mean=H_mean,
                                 H_median=H_median,
-                                H_q25=H_q25,
-                                H_q75=H_q75,
-                                H_q95=H_q95,
                                 V_std=V_std,
                                 V_var=V_var,
-                                V_sk=V_sk,
-                                V_ku=V_ku,
                                 C_ppr=C_ppr,
                                 A_std=A_std,
-                                A_med=A_med,
                                 A_cover=A_cover,
                                 nofveg=nofveg,
                                 nofallp=nofallp
@@ -98,7 +86,7 @@ for (i in objname) {
 
 dpcloudfea_exp_df_c=dpcloudfea_exp_df[complete.cases(dpcloudfea_exp_df), ]
 
-#write.csv(dpcloudfea_exp_df,paste("Balaton_lidarmetrics_",rad,"_reclass2.csv",sep=""))
+write.csv(dpcloudfea_exp_df,paste("Balaton_lidarmetrics_",rad,"_reclass3.csv",sep=""))
 #write.csv(dpcloudfea_exp_df_c,paste("Tisza_lidarmetrics_",rad,"_reclass2.csv",sep=""))
 #write.csv(dpcloudfea_exp_df_c,paste("Tisza_lidarmetrics_",rad,"_leafon_reclass2.csv",sep=""))
-write.csv(dpcloudfea_exp_df,paste("Ferto_lidarmetrics_",rad,"_reclass2.csv",sep=""))
+#write.csv(dpcloudfea_exp_df,paste("Ferto_lidarmetrics_",rad,"_reclass2.csv",sep=""))
