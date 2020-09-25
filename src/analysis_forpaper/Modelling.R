3a146ed164398cea563b2e6f7688a01401878720
komazsofi <komazsofi@gmail.com>
2020-09-25 09:21:29 +0200

    start visualize

--- src/analysis_forpaper/Modelling.R
+++ src/analysis_forpaper/Modelling.R
@@ -21,7 +21,7 @@ plot_data05$total.weight=plot_data05$total.weight/10000
 plot_data05_scaled=scale(plot_data05[,c(3:10)])
 colnames(plot_data05_scaled)=paste("Scaled_",colnames(plot_data05_scaled),sep="")
 plot_data05_f=cbind(plot_data05,plot_data05_scaled)
-plot_data05_f=plot_data05_f[(plot_data05_f$OBJNAME!=120 & plot_data05_f$OBJNAME!=209),]
+#plot_data05_f=plot_data05_f[(plot_data05_f$OBJNAME!=120 & plot_data05_f$OBJNAME!=209),]
 
 plot_data2=read.csv(paste("Plot_db_",2.5,".csv",sep=""))
 plot_data2$total.weight=plot_data2$total.weight/10000
@@ -29,7 +29,7 @@ plot_data2$total.weight=plot_data2$total.weight/10000
 plot_data2_scaled=scale(plot_data2[,c(3:10)])
 colnames(plot_data2_scaled)=paste("Scaled_",colnames(plot_data2_scaled),sep="")
 plot_data2_f=cbind(plot_data2,plot_data2_scaled)
-plot_data2_f=plot_data2_f[(plot_data2_f$OBJNAME!=120 & plot_data2_f$OBJNAME!=209),]
+#plot_data2_f=plot_data2_f[(plot_data2_f$OBJNAME!=120 & plot_data2_f$OBJNAME!=209),]
 
 plot_data5=read.csv(paste("Plot_db_",5,".csv",sep=""))
 plot_data5$total.weight=plot_data5$total.weight/10000
@@ -37,7 +37,7 @@ plot_data5$total.weight=plot_data5$total.weight/10000
 plot_data5_scaled=scale(plot_data5[,c(3:10)])
 colnames(plot_data5_scaled)=paste("Scaled_",colnames(plot_data5_scaled),sep="")
 plot_data5_f=cbind(plot_data5,plot_data5_scaled)
-plot_data5_f=plot_data5_f[(plot_data5_f$OBJNAME!=120 & plot_data5_f$OBJNAME!=209),]
+#plot_data5_f=plot_data5_f[(plot_data5_f$OBJNAME!=120 & plot_data5_f$OBJNAME!=209),]
 
 ####################################### Correlation check
 col <- colorRampPalette(c("#4477AA","#77AADD","#FFFFFF","#EE9988","#BB4444"))
@@ -197,3 +197,61 @@ summary(model_fwf_step_b5)
 
 stargazer(model_fwf_step_h5,model_all_step_h5,model_fwf_step_b5,model_all_step_b5,align=TRUE,type="text",column.labels=c("fwf","all","fwf","all"),title="5 m")
 
+####################################### Visualization
+
+##### 0.5 m
+# height
+
+#biomass
+
+
+##### 2.5 m
+
+plot_data2_f=plot_data2_f[(plot_data2_f$OBJNAME!=120 & plot_data2_f$OBJNAME!=163 & plot_data2_f$OBJNAME!=209),]
+plot_data2_f$type="not known"
+plot_data2_f[plot_data2_f$lake=="Lake Balaton",29]<- "dr"
+plot_data2_f[plot_data2_f$lake=="Lake Ferto",29]<- "fwf"
+plot_data2_f[plot_data2_f$lake=="Lake Tisza",29]<- "fwf"
+
+# height
+
+#biomass
+ggplot(data=plot_data2_f, aes(x=Scaled_V_var , y=total.weight),show.legend = TRUE) +  
+  geom_point(aes(colour=lake,shape=veg_type_2),size=5) +
+  geom_smooth(method="lm",aes(color=lake),se=FALSE)+
+  geom_smooth(method="lm",color="black",se=FALSE)+
+  theme_bw(base_size = 20) +
+  ylab("Biomass (field)") +
+  xlab("V_Var (scaled)")+
+  geom_text(aes(label=OBJNAME),hjust=0, vjust=0,size=2)+
+  ggtitle("LiDAR metrics for estimating biomass (2.5 m)")+
+  scale_colour_manual(values=c("Lake Balaton"="red", "Lake Ferto"="darkgreen","Lake Tisza"="blue"),name="Lakes and types")+
+  scale_shape_manual(values=c("carex"=16,"phragmites"=17,"typha"=15),name="Species",labels=c("Carex spec.","Phragmites australis","Typha spec."))
+
+ggplot(data=plot_data2_f, aes(x=Scaled_C_ppr , y=total.weight),show.legend = TRUE) +  
+  geom_point(aes(colour=lake,shape=veg_type_2),size=5) +
+  geom_smooth(method="lm",aes(color=type),se=FALSE)+
+  theme_bw(base_size = 20) +
+  ylab("Biomass (field)") +
+  xlab("C_ppr (scaled)")+
+  geom_text(aes(label=OBJNAME),hjust=0, vjust=0,size=2)+
+  ggtitle("LiDAR metrics for estimating biomass (2.5 m)")+
+  scale_colour_manual(values=c("Lake Balaton"="red", "Lake Ferto"="darkgreen","Lake Tisza"="blue"),name="Lakes")+
+  scale_shape_manual(values=c("carex"=16,"phragmites"=17,"typha"=15),name="Species",labels=c("Carex spec.","Phragmites australis","Typha spec."))
+
+ggplot(data=plot_data2_f, aes(x=Scaled_A_std , y=total.weight),show.legend = TRUE) +  
+  geom_point(aes(colour=lake,shape=veg_type_2),size=5) +
+  geom_smooth(method="lm",aes(color=type),se=FALSE)+
+  theme_bw(base_size = 20) +
+  ylab("Biomass (field)") +
+  xlab("A_std (scaled)")+
+  geom_text(aes(label=OBJNAME),hjust=0, vjust=0,size=2)+
+  ggtitle("LiDAR metrics for estimating biomass (2.5 m)")+
+  scale_colour_manual(values=c("Lake Balaton"="red", "Lake Ferto"="darkgreen","Lake Tisza"="blue"),name="Lakes")+
+  scale_shape_manual(values=c("carex"=16,"phragmites"=17,"typha"=15),name="Species",labels=c("Carex spec.","Phragmites australis","Typha spec."))
+
+
+##### 5 m
+# height
+
+#biomass
